7325326881935b1a435f616afa4563b97e6293e0
Benji314 <benjaminperreau1@gmail.com>
2023-09-02 20:27:31 +0200

    Modification du js de la page détail produit pour afficher selon l'id :bug: url à revoir

--- js/detailproduct1.js
+++ js/detailproduct1.js
@@ -1,5 +1,5 @@
-
-function generateDetailProductsHtml(products) {
+/* 
+function generateDetailProductHtml(products) {
     let detail_product = document.querySelector('.detail_product');
     products.forEach(function (product) {
         detail_product.innerHTML += `
@@ -52,7 +52,7 @@ function generateDetailProductsHtml(products) {
 
 function init(detail_data) {
     // 1 - Création de l'HTML
-    generateDetailProductsHtml(detail_data.products)
+    generateDetailProductHtml(detail_data.products)
 
 }
 
@@ -60,15 +60,78 @@ function init(detail_data) {
     .then((response) => response.json())
     .then((detail_data) => init(detail_data)) */
 
-    
-const url = new URL(window.location.href);
+
+/* const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
 
-if (id) {
-    //  Fichier JSON a un nom qui inclut l'ID : "product1.json" pour ID = 1.
-    fetch(`/json/product${id}.json`) 
-        .then((response) => response.json())
-        .then((detail_data) => generateDetailProductHtml(detail_data));
-} else {
-    console.error("ID not specified or invalid.");
+
+fetch(`/json/product${id}.json`)
+    .then((response) => response.json())
+    .then((detail_data) => generateDetailProductHtml(detail_data));  */
+
+
+function generateDetailProductHtml(product) {
+    let detail_product = document.querySelector('.detail_product');
+
+    detail_product.innerHTML = `
+            <div class="row row_class">
+                <div class="col-md-6 col_class">
+                    <div class="graine-cafe">
+                        <a href=""><img src="${product.path_img1}" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-md-5 offset-md-1 col_class description_product">
+                    <h1>${product.name}</h1>
+                    <p>${product.description}</p>
+                    <h2>€ ${product.unitPrice} <span class="incl_tva">incl. TVA</span></h2>
+                    <div class="add_cart_div">
+                        <button class="add_cart_button">AJOUTER AU PANIER </button>
+                    </div>
+                    <div class="flex share_div">
+                        <i class="fa-solid fa-share-nodes"></i>
+                        <h3>PARTAGER</h3>
+                        <span class="vertical-line2"></span>
+                        <p>Ref. ${product.ref}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row row_class">
+                <div class="col-md-3 col_class">
+                    <a href=""><img src="${product.path_img2}" alt=""></a>
+                </div>
+                <div class="col-md-3 col_class">
+                    <a href=""><img src="${product.path_img3}" alt=""></a>
+                </div>
+            </div>
+            <div class="row row_class">
+                <div class="col-md-6 col_class">
+                    <a href=""><img src="${product.path_img4}" alt=""></a>
+                </div>
+            </div>
+        `;
 }
+
+function init(detail_data, productId) {
+
+    const product = detail_data.products.find(product => product.id === productId);
+
+    if (product) {
+        generateDetailProductHtml(product);
+    } else {
+        console.error("Erreur de chargement");
+    }
+}
+
+/* // Obtenir l'ID du produit depuis l'URL :
+function getProductIdFromUrl() {
+    const searchParams = new URLSearchParams(window.location.search);
+    return searchParams.get('id');
+}
+
+const productId = getProductIdFromUrl(); */
+
+const productId = 5; 
+
+fetch('/json/products.json')
+    .then((response) => response.json())
+    .then((detail_data) => init(detail_data, productId));
